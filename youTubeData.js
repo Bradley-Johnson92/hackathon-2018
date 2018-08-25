a9c65673bff425e3bc23df8b277e6b752fa53329
Brad Johnson <bradjjohnson92@gmail.com>
2018-08-25 15:58:02 -0700

    changes

--- youTubeData.js
+++ youTubeData.js
@@ -19,7 +19,7 @@ const youTubeData = {
                 "publishedAt": "2018-03-21T16:51:35.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "Hotel Transylvania 3: Summer Vacation Trailer #2 (2017) | Movieclips Trailers",
-                "description": "Check out the official Hotel Transylvania 3: Summer Vacation trailer starring Adam Sandler! Let us know what you think in the comments below. ▻ Buy Tickets to ...",
+                "description": "Check out the official Hotel Transylvania 3: Summer Vacation trailer starring Adam Sandler! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/XVXBaE_piSs/default.jpg",
@@ -52,7 +52,7 @@ const youTubeData = {
                 "publishedAt": "2018-02-14T15:39:44.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "Uncle Drew Teaser Trailer #1 (2018) | Movieclips Trailers",
-                "description": "Check out the official Uncle Drew teaser trailer starring Shaquille O'Neal! Let us know what you think in the comments below. ▻ Buy Tickets to Uncle Drew: ...",
+                "description": "Check out the official Uncle Drew teaser trailer starring Shaquille O'Neal! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/Uv1xscwDtIQ/default.jpg",
@@ -85,7 +85,7 @@ const youTubeData = {
                 "publishedAt": "2013-04-15T18:47:38.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "The Kings of Summer Official Trailer #1 (2013) - Nick Offerman, Alison Brie Movie HD",
-                "description": "Subscribe to TRAILERS: http://bit.ly/sxaw6h Subscribe to COMING SOON: http://bit.ly/H2vZUn Like us on FACEBOOK: http://goo.gl/dHs73 The Kings of Summer ...",
+                "description": "Subscribe to TRAILERS: http://bit.ly/sxaw6h Subscribe to COMING SOON: http://bit.ly/H2vZUn Like us on FACEBOOK: http://goo.gl/dHs73",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/cio8LOCZPzw/default.jpg",
@@ -118,7 +118,7 @@ const youTubeData = {
                 "publishedAt": "2018-04-05T08:02:33.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "Johnny English Strikes Again Trailer #1 (2018) | Movieclips Trailers",
-                "description": "Check out the official Johnny English Strikes Again trailer starring Rowan Atkinson! Let us know what you think in the comments below. ▻ Buy Tickets to Johnny ...",
+                "description": "Check out the official Johnny English Strikes Again trailer starring Rowan Atkinson! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/9cBN9_9oK4A/default.jpg",
@@ -151,7 +151,7 @@ const youTubeData = {
                 "publishedAt": "2018-06-05T13:26:14.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "The Lego Movie 2: The Second Part Teaser Trailer #1 (2018) | Movieclips Trailers",
-                "description": "Check out the official The Lego Movie 2: The Second Part teaser trailer starring Chris Pratt! Let us know what you think in the comments below. ▻ Buy Tickets to ...",
+                "description": "Check out the official The Lego Movie 2: The Second Part teaser trailer starring Chris Pratt! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/S-36pC4PZ18/default.jpg",
@@ -184,7 +184,7 @@ const youTubeData = {
                 "publishedAt": "2018-04-25T13:55:41.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "Smallfoot Trailer #1 (2018) | Movieclips Trailers",
-                "description": "Check out the official Smallfoot trailer starring Channing Tatum! Let us know what you think in the comments below. ▻ Buy Tickets to Smallfoot: ...",
+                "description": "Check out the official Smallfoot trailer starring Channing Tatum! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/chPeYcLvCpI/default.jpg",
@@ -217,7 +217,7 @@ const youTubeData = {
                 "publishedAt": "2013-06-20T17:28:49.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "The Spectacular Now Official Trailer #1 (2013) - Shailene Woodley Movie HD",
-                "description": "Subscribe to TRAILERS: http://bit.ly/sxaw6h Subscribe to COMING SOON: http://bit.ly/H2vZUn Like us on FACEBOOK: http://goo.gl/dHs73 The Spectacular Now ...",
+                "description": "Subscribe to TRAILERS: http://bit.ly/sxaw6h Subscribe to COMING SOON: http://bit.ly/H2vZUn Like us on FACEBOOK: http://goo.gl/dHs73",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/XDTBLSkUmYk/default.jpg",
@@ -250,7 +250,7 @@ const youTubeData = {
                 "publishedAt": "2012-12-03T19:25:54.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "Love Me Official Blu-ray Trailer #1 (2013) - Lindsey Shaw, Jean-Luc Bilodeau Movie HD",
-                "description": "Subscribe to TRAILERS: http://bit.ly/sxaw6h Subscribe to COMING SOON: http://bit.ly/H2vZUn Love Me Official Blu-ray Trailer #1 (2013) - Lindsey Shaw, ...",
+                "description": "Subscribe to TRAILERS: http://bit.ly/sxaw6h Subscribe to COMING SOON: http://bit.ly/H2vZUn Love Me Official Blu-ray Trailer #1 (2013)",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/176G-2jsId4/default.jpg",
@@ -283,7 +283,7 @@ const youTubeData = {
                 "publishedAt": "2018-05-07T15:53:45.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "Hotel Transylvania 3: Summer Vacation Teaser Trailer #1 (2018) | Movieclips Trailers",
-                "description": "Check out the official Hotel Transylvania 3: Summer Vacation trailer starring Adam Sandler! Let us know what you think in the comments below. ▻ Buy Tickets to ...",
+                "description": "Check out the official Hotel Transylvania 3: Summer Vacation trailer starring Adam Sandler! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/WjpdpeuGwaY/default.jpg",
@@ -316,7 +316,7 @@ const youTubeData = {
                 "publishedAt": "2018-03-08T16:58:21.000Z",
                 "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
                 "title": "The Grinch Trailer #1 (2018) | Movieclips Trailers",
-                "description": "Check out the official The Grinch trailer starring Benedict Cumberbatch! Let us know what you think in the comments below. ▻ Buy Tickets to The Grinch: ...",
+                "description": "Check out the official The Grinch trailer starring Benedict Cumberbatch! Let us know what you think in the comments below.",
                 "thumbnails": {
                     "default": {
                         "url": "https://i.ytimg.com/vi/knFho9WzLJQ/default.jpg",
